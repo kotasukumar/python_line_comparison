c80dd740f6b2f973678974bb61fcba3d41672cad
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-05-18 19:43:22 +0530

    length

--- line_comparision.py
+++ line_comparision.py
@@ -1,1 +1,11 @@
-print("Welcome to line comparison program")
+import math
+
+
+def line(x1, x2, y1, y2):
+    length = math.sqrt(math.pow((x2 - x1), 2)) + math.sqrt(math.pow((y2 - y1), 2))
+    return length
+
+
+if __name__ == "__main__":
+    l1 = line(5, 6, 15, 21)
+    print(l1)
