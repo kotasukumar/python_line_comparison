c7aebaf9c222d92cb369acd780bf396bff65c1d5
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-05-18 19:48:50 +0530

    comparing

--- line_comparision.py
+++ line_comparision.py
@@ -1,6 +1,13 @@
 import math
 
 
+def check(length1, length2):
+    if length1 == length2:
+        return "Both are equal"
+    else:
+        return "Both are not equal"
+
+
 def line(x1, x2, y1, y2):
     length = math.sqrt(math.pow((x2 - x1), 2)) + math.sqrt(math.pow((y2 - y1), 2))
     return length
@@ -8,4 +15,6 @@ def line(x1, x2, y1, y2):
 
 if __name__ == "__main__":
     l1 = line(5, 6, 15, 21)
-    print(l1)
+    l2 = line(5, 6, 11, 19)
+    result = check(l1, l2)
+    print(result)
